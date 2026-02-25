f38ee82c80e26d1b90ef6777f2e5f6b9c4660bc0
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-02-25 19:15:15 +0530

    learning java sacript

--- function.js
+++ function.js
@@ -27,3 +27,55 @@
 //3. without argument and return value
 //4. with argument and return value
 
+// arguments/parameter :- 
+// function fun1(parameter){
+//        //code
+// } 
+
+// fun1(value(argument/actual_parameter))  //formal parameter
+
+
+// function demo(a){
+//     return a+a
+// }
+// console.log(demo(8))
+// let z = demo(9)
+// console.log(z)
+
+//2. with argument and no return value
+// function demo(a,b){
+//     console.log(a*b); 
+// }
+// let a = parseInt(prompt("Enter a number: "))
+// let b = parseInt(prompt("Enter a number: "))
+// demo(a,b)
+
+// wap to print  a table of any number using function type two
+// function table(a){
+//     for(let x=1; x<=10; x++){
+//         console.log(a*x)
+//     }
+// }
+// let i = parseInt(prompt("Enter any number for table: "))
+// table(i)
+
+//3. without argument and return value
+// function demo(){
+//     let a = parseInt(prompt("Enter any number: "))
+//     return a+a
+// }
+// console.log(demo())
+
+// function demo(){
+//     return "Java Script"
+// }
+// console.log(demo());
+
+
+//4. with argument and return value
+// function demo(a,b){
+//     return a+b
+// }
+// let a= parseInt(prompt("Enter 1st number: "))
+// let b= parseInt(prompt("Enter 2nd number: "))
+// console.log(demo(a,b))
