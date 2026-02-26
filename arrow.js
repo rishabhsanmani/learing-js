8ab7efeb2c978096f2c9e8022598d06798d824ed
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-02-26 13:08:43 +0530

    learning java script

---
+++ arrow.js
@@ -0,0 +1,30 @@
+// Arrow Function :-  it comes in 2015 ES6. it is made up for one liner function it is a anonymu function 
+// we have to store arrow function in a variable to call it. when we don't pass argument it is compulsory to put parenthesis 
+
+// Named function :- uit has name after function keyword
+// Anonymus function :- on this function has no name
+
+// syntax :-
+
+// 1. ()=>{return }      // basic syntax of arrow function 
+// 2. ()=>()
+// 3. ()=>
+//    =>
+//    =>()
+
+// let fun = ()=>{return "Code"}
+// console.log(fun());
+
+// let sum = (a,b)=>{return a+b}
+// console.log(sum(12,3))
+
+// let s = (a)=>(a+a)
+// console.log(s(20));
+
+// let s = (a)=>("code")
+// let an = s()
+// console.log(an);
+
+// let demo = x => x+x
+// console.log(demo(2));
+
