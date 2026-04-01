eec6cde15f003a2cdac2e78f76297c049244d168
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-04-01 13:21:24 +0530

    API

---
+++ api.js
@@ -0,0 +1,15 @@
+// api :- application programming interface it is use to run third-party application in our webpage
+
+// js is a light weight language it works fast it is a both syncronas and asyncronas
+// js work like async when we use api
+
+// syncronas programming language :- it execute line by line whether the line is taking 10 second or 10 minutes to execute
+// asyncronas programming language :- it execute the line first which take less time to execute
+
+// syntax :- fetch(URL,{methonds(http)})
+
+// url :- uniform resource locator 
+
+// http(hypertext transfer protocol) method :- get: to access , post: to insert , delete: to remove , put: to update , patch: to update 
+// https (hypertext transfer protocol secure) 
+
